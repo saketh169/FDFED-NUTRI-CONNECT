585d76a73d94336299590ddbd8e3ace4286cca1b
saketh169 <sakethrupa123@gmail.com>
2025-12-18 16:43:44 +0530

    NUTRI-CONNECT

--- backend/src/server.js
+++ backend/src/server.js
@@ -103,7 +103,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 // SPA routing - serve index.html for all non-API routes
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 
